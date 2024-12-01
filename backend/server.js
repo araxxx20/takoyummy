d1ff64995e92220ecf10d8bf169076a920d9cf23
MRV-dev <a.marv0903@gmail.com>
2024-12-01 09:04:11 +0800

    hdhgcf

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://127.0.0.1:5502',  
+    origin: 'http://127.0.0.1:5503',  
     methods: ['GET', 'POST'],  
     allowedHeaders: ['Content-Type'],        
     credentials: true    
